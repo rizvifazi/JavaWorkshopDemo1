cb39f8e2ac66362d96ad898afa18682f5c6905bd
Rizvi Fazi <rizvifazi124@gmail.com>
2023-11-04 06:58:14 +0530

    First commit

---
+++ src/Man.java
@@ -0,0 +1,13 @@
+public class Man {
+
+    private int age = 25;
+    private String name = "Rizvi";
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
